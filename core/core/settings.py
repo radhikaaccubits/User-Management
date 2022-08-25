6558068ce8abd4c81e5b9d4bfa06f304ae7bd2c5
gurrappa-c <110536039+gurrappa-c@users.noreply.github.com>
2022-08-25 10:16:46 +0530

    email credentials removed

--- core/core/settings.py
+++ core/core/settings.py
@@ -141,9 +141,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/login/"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'chinthapandu.g@accubits.com'
-EMAIL_HOST_PASSWORD = 'Gurrappa@123'
-EMAIL_PORT = 587
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
