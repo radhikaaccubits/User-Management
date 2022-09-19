b11b2496fddfc32ba1396be577cc1be16e05459a
ShreyVij <109672232+ShreyVij@users.noreply.github.com>
2022-09-19 13:52:04 +0530

    Made required changes

--- core/web/router.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from .views import role_viewset,userprofile_viewset
-
-router=routers.DefaultRouter()
-router.register('role',role_viewset)
-router.register('user',userprofile_viewset)
