dd157a8362c74323d607916f76ed5a4991ab9844
ShreyVij <109672232+ShreyVij@users.noreply.github.com>
2022-09-20 13:54:01 +0530

    Made required changes

--- core/core/settings.py
+++ core/core/settings.py
@@ -83,6 +83,14 @@ TEMPLATES = [
         },
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
 
 WSGI_APPLICATION = "core.wsgi.application"
 
